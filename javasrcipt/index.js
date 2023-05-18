5991e4e7ca9c8c92a28cfd6d60363ef46e67cf2c
Dek ibrahim muse <122024928+Dek-ibrahim@users.noreply.github.com>
2023-05-18 07:42:54 +0300

    Add files via upload

---
+++ javasrcipt/index.js
@@ -0,0 +1,127 @@
+const names = [];
+names[0]= "The Beneficent";
+names[1]= "The Merciful";
+names[2]= "The King";
+names[3]= "The Most Sacred";
+names[4]= "The Source of Peace, The Flawless";
+names[5]= "The Infuser of Faith";
+names[6]= "AL-MUHAYMIN	The Preserver of Safety";
+names[7]= "All Mighty";
+names[8]= "The Compeller, The Restorer";
+names[9]= "The Supreme, The Majestic";
+names[10]= "The Creator, The Maker";
+names[11]= "The Evolver";
+names[12]= "The Fashioner";
+names[13]= "The Constant Forgiver";
+names[14]= "The All-Prevailing One";
+names[15]= "The Provider";
+names[16]= "The Supreme Solver";
+names[17]= "The All-Knowing";
+names[18]= "The Withholder";
+names[19]= "The Extender";
+names[20]= "The Reducer";
+names[21]= "The Exalter, The Elevator";
+names[22]= "The Honourer, The Bestower";
+names[23]= "The Dishonourer ";
+names[24]= " The All-Hearing";
+names[25]= "  The All-Seeing";
+names[26]= " The Impartial Judge";
+names[27]= "the Utterly Just";
+names[28]= "The Subtle One, The Most Gentle";
+names[29]= " The All-Aware";
+names[30]= " 	The Most Forbearing";
+names[31]= "The Magnificent, The Supreme";
+names[32]= "The Most Appreciative";
+names[33]= "The Most High, The Exalted";
+names[34]= "";
+names[35]= "";
+names[36]= "";
+names[37]= "";
+names[38]= "";
+names[39]= "";
+names[40]= "";
+names[40]= "";
+names[40]= "";
+names[40]= "";
+names[40]= "";
+names[40]= "";
+names[40]= "";
+names[40]= "";
+names[40]= "";
+
+console.log(names);
+
+// 	
+// 	
+//
+// 
+// 
+// 
+// 	
+// 
+// 	
+// 		
+// 	
+// 	
+// 		The Preserver
+// 	The Sustainer
+// 	The Reckoner
+// 	The Majestic
+// 	The Most Generous, The Most Esteemed
+// 	The Watchful
+// 	The Responsive One
+// 	The All-Encompassing, the Boundless
+// 	The All-Wise	The Most Loving
+// 	The Glorious, The Most Honorable
+// 	The Infuser of New Life
+// 	The All Observing Witnessing
+//  the Absolute Truth
+// 		The Trustee, The Disposer of Affairs
+// 	The All-Strong
+// 		The Firm, The Steadfast
+// 	The Protecting Associate
+// 		The Praiseworthy
+// 		The All-Enumerating, The Counter
+// 	The Originator, The Initiator
+// 	The Restorer, The Reinstater
+// 	The Giver of Life
+// 		The Creator of Death
+// The Ever-Living
+// 		The Sustainer, The Self-Subsisting
+// 		The Perceiver
+// 		The Illustrious, the Magnificent
+// 	The One
+//  The Only One
+// 	The Eternal, Satisfier of Needs
+// 		The Omnipotent One
+// 	The Powerful
+// 	The Expediter, The Promoter
+// 	The Delayer
+// 	The First
+// 	The Last
+// 	The Manifest
+//   The Hidden One, Knower of the Hidden
+// 	The Sole Governor
+//   	The Self Exalted
+//   the Source of All Goodness
+//  The Ever-Pardoning
+//  The Avenger
+// 	The Pardoner
+//  The most Kind
+// 	 the Master of the Kingdom, Owner of the Dominion
+// 	Lord of Glory and Honour, Lord of Majesty and Generosity
+// 		The Just One
+// 			The Gatherer, the Uniter
+// 			The Self-Sufficient, The Wealthy
+// 		The Enricher
+// 		The Withholder
+// 			The Distresser
+// 		The Propitious, the Benefactor
+// 			The Light, The Illuminator
+//  	The Guide
+// The Incomparable Originator
+// 	the Everlasting
+// 	The Inheritor, The Heir
+// 	The Guide, Infallible Teacher
+// The Forbearing, The Patien
+
